dac47cd025550d9c75f52109127653e90d42e559
Jolan Levy <jolan.levy@gmail.com>
2015-10-08 10:53:30 +0200

    first commit

---
+++ suppression_collection.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require('include/connexion.php');
+
+$bSupprimer = false;
+/**
+ * Vérifie si un identifiant de collection est fourni
+ * et si celui-ci est bien un entier
+ */
+$iIdentifiant = filter_var($_GET['id'], FILTER_VALIDATE_INT);
+if ( isset($_SESSION['token']) && isset($_GET['token']) ) {
+    if ($_SESSION['token'] == $_GET['token']) {
+        if (isset($_GET['id']) && false !== $iIdentifiant) :
+            /**
+             * Supprime les ouvrages de la collection
+             */
+            $sRequeteSql = 'DELETE FROM ouvrage WHERE collection_id = ' . $iIdentifiant;
+            $oConnexion->query($sRequeteSql);
+
+            /**
+             * Supprime la collection
+             */
+            $sRequeteSql = 'DELETE FROM collection WHERE id = ' . $iIdentifiant;
+            $oConnexion->query($sRequeteSql);
+            $bSupprimer = true;
+        endif;
+    }
+}
+
+header('Location: index.php?page=collection.php&etat_suppression=' . (int) $bSupprimer);
